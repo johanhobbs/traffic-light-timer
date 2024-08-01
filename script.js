c41fbaac8e424b3bb17cce450ee5b3ac90f69fdc
johanhobbs <139718130+johanhobbs@users.noreply.github.com>
2024-08-02 10:53:38 +1200

    Basic elements added, basic CSS, JavaScript not yet working

---
+++ script.js
@@ -0,0 +1,9 @@
+const greenLight = document.getElementById(green);
+const amberLight = document.getElementById(amber);
+const redLight = document.getElementById(red);
+
+window.onload = function () {
+  greenLight.style.backgroundColor = 'green';
+  amberLight.style.backgroundColor = '#FFBB00';
+  redLight.style.backgroundColor = 'red';
+};
